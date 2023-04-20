fedd5bc0a2ff55eff3bdf279e08f28f3b9347e41
shivu <shivanithoke3108@gmail.com>
2023-04-20 22:25:45 +0530

    solution1

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+import logo from './logo.svg';
+
+export default function App() {
+  return (
+    
+    <div ClassName="App">
+      <header ClassName="App-header">
+    
+      <img src={logo} ClassName="App-logo" alt="logo" />
+      
+    <h1>Benefits of React...</h1>
+        <ol>
+          <li>Component-based architecture</li>
+          <li>Virtual DOM for efficient updates</li>
+          <li>Rich ecosystem and community</li>
+          <li>Cross-platform development</li>
+          <li>Strong community support:</li>
+          </ol>
+         
+      <button>Get Started...</button>
+      </header>
+      </div> 
+          
+        
+      
+          )
+    }
+          
+          
+  
+
+
