f1f22fd492cc6912f5954251ae494bbf8ec23730
shivu <shivanithoke3108@gmail.com>
2023-04-21 20:34:37 +0530

    Solution2

---
+++ src/Component/ReactBenefits.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Fragment } from 'react'
+export default function ReactBenefits() {
+  return (
+    <Fragment>
+    <h1>Benefits of using react...</h1>
+    <ol>
+        <li>Component-based architecture</li>
+        <li>Virtual DOM for efficient updates</li>
+        <li>Rich ecosystem and community</li>
+        <li>Cross-platform development</li>
+        <li>Strong community support</li>
+    </ol>
+    </Fragment>
+  )
+}
---
+++ src/Component/ReactLogo.js
@@ -0,0 +1,8 @@
+import React from 'react'
+ 
+export default function ReactLogo()
+ {
+    return(
+  <img src="./logo192.png"></img>
+    )
+ }
